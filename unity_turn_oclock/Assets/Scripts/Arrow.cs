5bda22b8ed054cc8c86d34d086cce35ea6cb6cf0
foryourselfand <foryourselfand@gmail.com>
2018-11-29 22:39:46 +0300

    Arrow moving

---
+++ unity_turn_oclock/Assets/Scripts/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float speed = 60;
+    private int direction = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        direction = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed * direction));
+    }
+}
